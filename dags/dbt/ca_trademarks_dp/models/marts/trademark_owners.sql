11ab5be8114c43219e4c871fa832defa0ef31fa4
wndrlxx <64194574+wndrlxx@users.noreply.github.com>
2024-04-16 12:13:31 -0700

    add documentation for marts and intermediate models

--- dags/dbt/ca_trademarks_dp/models/marts/trademark_owners.sql
+++ dags/dbt/ca_trademarks_dp/models/marts/trademark_owners.sql
@@ -1,5 +1,5 @@
 select party_name, count(*) as count
-from {{ ref("int_application_main_filtered_by_active") }}
+from {{ ref("int_applications_filtered_by_active") }}
 join {{ ref("stg_cipo__interested_parties") }} using (application_id)
 where party_type_description = "Registrant"
 group by party_name
